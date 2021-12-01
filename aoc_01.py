87bb84a5cd90fed7b546d889866ff737e5a0c6fc
Martin Aregger <martinaregger@protonmail.com>
2021-12-01 11:54:00 +0100

    actually using my brain

--- aoc_01.py
+++ aoc_01.py
@@ -1,16 +1,11 @@
-import statistics
 
-def solution_1(input_list: list) -> int:
-    return sum(a<b for a,b in zip(input_list,input_list[1:]))
-
-
-if __name__ == "__main__":
-    with open("/mnt/z/vscode_wsl_data/aoc_2021/Data/aoc_01a.txt", "r") as aoc_01_data:
-        lines = [int(n) for n in aoc_01_data.readlines()]
-        
-    # Part 1
-    print(solution_1(lines))
+with open("Data/aoc_01a.txt", "r") as aoc_01_data:
+    lines = [int(n) for n in aoc_01_data.readlines()]
     
-    # Part 2
-    print(solution_1([statistics.mean([a,b,c]) for a,b,c in zip(lines,lines[1:],lines[2:])]))
-    
+# Part 1
+print(sum(a<b for a,b in zip(lines,lines[1:]))
+
+)
+
+# Part 2
+print(sum(a<b for a,b in zip(lines,lines[3:])))
