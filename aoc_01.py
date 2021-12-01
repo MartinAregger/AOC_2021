f49ebe18f24244f968e6e283fc73b236c28b04d5
Martin Aregger <martinaregger@protonmail.com>
2021-12-01 11:54:46 +0100

    bracket

--- aoc_01.py
+++ aoc_01.py
@@ -3,9 +3,7 @@ with open("Data/aoc_01a.txt", "r") as aoc_01_data:
     lines = [int(n) for n in aoc_01_data.readlines()]
     
 # Part 1
-print(sum(a<b for a,b in zip(lines,lines[1:]))
-
-)
+print(sum(a<b for a,b in zip(lines,lines[1:])))
 
 # Part 2
 print(sum(a<b for a,b in zip(lines,lines[3:])))
